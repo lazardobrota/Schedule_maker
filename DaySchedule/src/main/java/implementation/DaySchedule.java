76283834113e0e65f35c6c215481901717443fe9
lazardobrota <ldobrota7421rn@raf.rs>
2023-10-28 11:25:00 +0200

    fixed bug to make classes with same adress and made method to find day with date

--- DaySchedule/src/main/java/implementation/DaySchedule.java
+++ DaySchedule/src/main/java/implementation/DaySchedule.java
@@ -1,8 +1,10 @@
 package implementation;
 
+import exceptions.InvalidDateException;
 import specification.Appointment;
 import specification.Room;
 import specification.Schedule;
+import specification.Time;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -26,38 +28,42 @@ public class DaySchedule extends Schedule {
         return this.getRooms().add(room); // returns true if element doesnt exist so its added to the list
     }
 
+    private LocalDate findDateWithDay(LocalDate startDate, int day) {
+        day %= 7; //it has 7 days in the week
+
+        if (day <= 0)
+            day += 7; // 0 Saturday, -1 Sunday,...
+
+        //How many days between startDate and day we want, it only works if day is ahead then startDate
+        int addDays = day - startDate.getDayOfWeek().getValue();
+
+        //If startDate is ahead then go backwords to the day given and add 7 day to go to the next week of that day
+        if (addDays < 0)
+            addDays += 7;
+
+        return startDate.plusDays(addDays);
+    }
+
+    //TODO This method is more for other implementation from to date
     @Override
-    public boolean addAppointment(Appointment appointment, int day, LocalDate startDate, LocalDate endDate) {
+    public boolean addAppointment(Appointment appointment, int day, LocalDate startDate, LocalDate endDate) throws InvalidDateException {
         List<Appointment> appointmentList = new ArrayList<>();
         int weeksBetween = (int) ChronoUnit.WEEKS.between(startDate, endDate);
 
         //Check rules
 
         //End date is before start date
-        if (weeksBetween < 0)
-            return false;
+        if (weeksBetween < 0) {
+            throw new InvalidDateException("Invalid startDate: " + startDate + " and endDate: "+ endDate);
+        }
 
-        day %= 7; //it has 7 days in the week
-
-        if (day <= 0)
-            day = 7 + day; // -1 will be Sunday
-
-        DayOfWeek dayOfWeek = DayOfWeek.of(day);//gets day of the week
-        LocalDate date = startDate;
-
-        //How many days should be added to startDate so it starts from given day
-        int addDays = dayOfWeek.getValue() - startDate.getDayOfWeek().getValue();
-
-        if (addDays < 0)
-            addDays = 7 + addDays;
-
-        date = startDate.plusDays(addDays);
+        LocalDate date = findDateWithDay(startDate, day);
 
         //Makes appointments
 
-        //TODO getTime and getRoom send pointer, it should be new instance
         for (int i = 0; i < weeksBetween; i++) {
-            Appointment appoint = new Appointment(appointment.getRoom(), appointment.getTime()); //Makes new appointment
+            //Makes new appointment, calls copy Constructor
+            Appointment appoint = new Appointment(new Room(appointment.getRoom()), new Time(appointment.getTime()));
             appoint.getTime().setDate(date); //sets its date
 
             //Already has one of the days as appointment so it fails
@@ -68,6 +74,7 @@ public class DaySchedule extends Schedule {
             date = date.plusDays(7);//goes to next week
         }
 
+        //All appointments are
         getAppointments().addAll(appointmentList);
         return true;
     }
