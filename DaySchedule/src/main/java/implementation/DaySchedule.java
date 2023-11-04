9c4b8935b040b28e21bf6840ffa69c59742f35ab
lazardobrota <ldobrota7421rn@raf.rs>
2023-11-04 16:16:49 +0100

    removeAppointment method done

--- DaySchedule/src/main/java/implementation/DaySchedule.java
+++ DaySchedule/src/main/java/implementation/DaySchedule.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -45,11 +46,32 @@ public class DaySchedule extends Schedule {
         }
 
         this.getAppointments().addAll(appointments);
+        Collections.sort(getAppointments());
         return true;
     }
 
     @Override
     public boolean removeAppointment(Appointment appointment, int day) throws InvalidDateException{
+        if (appointment.getTime().getStartDate().getDayOfWeek().getValue() != day)
+            throw new InvalidDateException("Day needs to be the same as startDate");
+
+        if (appointment.getTime().getEndDate().isBefore(appointment.getTime().getStartDate()))
+            throw new InvalidDateException("startDate need to be before endDate");
+
+        List<Appointment> appointments = makeOneFromMultiDay(new Appointment(new Room(appointment.getRoom()), new Time(appointment.getTime())));
+        List<Appointment> toRemove = new ArrayList<>();
+        for (Appointment a : appointments) {
+            //Date isn't good
+            if (!isValidDate(a.getTime().getStartDate()))
+                return false;
+
+            //It exists in the list
+            if (this.getAppointments().contains(a))
+                toRemove.add(a);
+        }
+
+        this.getAppointments().removeAll(toRemove);
+        Collections.sort(this.getAppointments());
         return true;
     }
 
--- DaySchedule/src/test/java/DayScheduleTest.java
+++ DaySchedule/src/test/java/DayScheduleTest.java
@@ -38,6 +38,32 @@ public class DayScheduleTest {
         assertThrows(InvalidDateException.class, () -> daySchedule.addAppointment(appointment, 3));
     }
 
+    @Test
+    public void removeAppointment() throws InvalidDateException {
+        Room room = new Room("raf1");
+        Time time = new Time(LocalDate.of(2023, 10, 16), LocalDate.of(2023, 10, 20), LocalTime.of(10, 0), LocalTime.of(12, 0));
+        Appointment appointment = new Appointment(room, time);
+
+        DaySchedule daySchedule = new DaySchedule(LocalDate.of(2023, 10, 10), LocalDate.of(2023, 11, 1));
+
+        //16.10.2023 to 20.10.2023. monday
+        daySchedule.addAppointment(appointment, 1);
+        println(daySchedule.getAppointments());
+
+        //17.10.2023 to 19.10.2023. tuesday
+        appointment.getTime().setStartDate(appointment.getTime().getStartDate().plusDays(1));
+        appointment.getTime().setEndDate(appointment.getTime().getEndDate().minusDays(1));
+        daySchedule.removeAppointment(appointment, 2);
+        println(daySchedule.getAppointments());
+        assertEquals(2, daySchedule.getAppointments().size());
+
+
+        //Same date, different day
+        appointment.getTime().setStartDate(LocalDate.of(2023, 10, 16));
+        appointment.getTime().setStartDate(LocalDate.of(2023, 10, 16));
+        assertThrows(InvalidDateException.class, () -> daySchedule.addAppointment(appointment, 3));
+    }
+
     private void println(List<Appointment> appointmentList) {
         for (Appointment a: appointmentList) {
             System.out.println(a);
